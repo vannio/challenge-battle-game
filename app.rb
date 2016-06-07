eda914b83397f2f9a2197c5b852948ac03e80b2e
joestephens <joe.stephens@live.co.uk>
2016-06-07 16:42:03 +0100

    Added sessions and redirects :squirrel:

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'shhhhhh'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:names)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
