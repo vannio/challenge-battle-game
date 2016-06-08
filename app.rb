afc891a51ab0e762cc3b98ae1daf5ab38674ba40
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-08 16:20:40 +0100

    Sessions

--- app.rb
+++ app.rb
@@ -3,17 +3,26 @@ require 'shotgun'
 require 'capybara'
 
 
+
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-    erb(:play)
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    erb(:play)
+  end  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
