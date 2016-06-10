9f08f5cd1728af25ae1c5cc3c1166072ce7c2fd2
srMarquinho <sr.marquinho@gmail.com>
2016-06-10 11:11:54 +0100

    :sparkles: :sparkles: :fire: :fire:, removed global variables

--- app.rb
+++ app.rb
@@ -12,20 +12,20 @@ class Battle < Sinatra::Base
   post '/names' do
     player1 = Player.new(params[:player1])
     player2 = Player.new(params[:player2])
-    $game = Game.new(player1, player2)
+    Game.start(player1, player2)
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = $game.players.first
-    @player2 = $game.players.last
+    @player1 = Game.current.players.first
+    @player2 = Game.current.players.last
     erb(:play)
   end
 
   post '/attack' do
-    @player1 = $game.players.first
-    @player2 = $game.players.last
-    $game.attack(@player2)
+    @player1 = Game.current.players.first
+    @player2 = Game.current.players.last
+    Game.current.attack(@player2)
     erb(:attack)
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,17 @@ class Game
 
   attr_reader :players
 
-  def initialize(player1=Player.new, player2=Player.new)
+  def initialize(player1, player2)
     @players = [player1, player2]
     @current_player = player1
+  end
+
+  def self.current
+    @@game
+  end
+
+  def self.start(player1=Player.new, player2=Player.new)
+    @@game = new(player1, player2)
   end
 
   def attack(player)
